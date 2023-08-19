7dd4682d6cea65f8b023468c974aeb18d554954b
Jonathan Cwengel <jonathancwengel@gmail.com>
2023-08-19 13:14:32 +0300

    fixing edge cases in SIGN UP js

--- Controllers/signUpCtrl.js
+++ Controllers/signUpCtrl.js
@@ -1,25 +1,24 @@
 const path = require('path');
 const bcrypt = require("bcryptjs");
 const User = require("../Backend/models/User");
-
 //----------------signUp page-----------------------//
 function getSignUpPage(req, res) {
     res.sendFile(path.join(__dirname, '..', 'View', 'signUp.html'));
 }
 
 async function setNewUsser(req,res){
+    const errorsArr = [];
     const fullname = req.body.fullname;
     const email = req.body.email;
     const password = req.body.password;
     console.log(email)
     
-
     let user = await User.findOne({ email });
     if (user){
         console.log("already exsit -> sign in")
-        return res.redirect("/signin");    
+        return res.redirect("/signup");            
     }
-    if (fullname.includes(" ") && email.includes("@") && email.includes(".") && password.length >= 6){
+    if (/^[a-zA-Z\s]*[a-zA-Z][a-zA-Z\s]*$/.test(fullname) && fullname.includes(" ") && email.includes("@") && email.includes(".") && password.length >= 6 && password.length <= 12 ){
         const hasdPsw = await bcrypt.hash(password, 12);
         user = new User({
             fullname,
@@ -32,14 +31,35 @@ async function setNewUsser(req,res){
             user.admin = true;
         }
         await user.save();
+        console.log("user was sucssefully created");
         const date = new Date(); // Set the desired expiration date
         date.setDate(date.getDate() + 30); // For example, set the cookie to expire in 30 days
         res.cookie("myCookie", "", { expires: new Date(date) }); 
         res.redirect("/signin");
     }else{
+        if (!fullname.includes(" ")) {
+            errorsArr.push("Last name required");
+            console.log("Last name required");
+        }
+        if (!/^[a-zA-Z\s]*[a-zA-Z][a-zA-Z\s]*$/.test(fullname)) {
+            errorsArr.push("Last name required");
+            console.log("Alphabet letter required");
+        }
+        if (!email.includes("@") || !email.includes(".")) {
+            errorsArr.push("Invalid Email");
+            console.log("Invalid Email");
+        }
+        if (password.length < 6 || password.length > 12) {
+            errorsArr.push("Password invalid");
+            console.log("Password invalid");
+        }
+        console.log("User was not created");
 		res.redirect("/signup");
+        //res.render("signup", { errorsArr });
     }
-
+    
 }
+
+
 
 module.exports = {getSignUpPage,setNewUsser};
