857856fbc83a119d2e6c1971473edb8afdfd5177
Jonathan Cwengel <jonathancwengel@gmail.com>
2023-08-22 13:02:44 +0300

    changes in singup logics

--- Controllers/signUpCtrl.js
+++ Controllers/signUpCtrl.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const bcrypt = require("bcryptjs");
 const User = require("../Backend/models/User");
-
 //----------------signUp page-----------------------//
 function getSignUpPage(req, res) {
     res.sendFile(path.join(__dirname, '..', 'View', 'signUp.html'));
 }
 
 async function setNewUsser(req,res){
+    const errorsArr = [];
     const fullname = req.body.fullname;
     const email = req.body.email;
     const password = req.body.password;
@@ -16,9 +16,9 @@ async function setNewUsser(req,res){
     let user = await User.findOne({ email });
     if (user){
         console.log("already exsit -> sign in")
-        return res.status(400).json({ error: "User already exists" });
-    }  
-    if (fullname.includes(" ") && email.includes("@") && email.includes(".") && password.length >= 6){
+        return res.redirect("email-exists.html"); // Redirect to the email-exists page            
+    }
+    if (/^[a-zA-Z\s]*[a-zA-Z][a-zA-Z\s]*$/.test(fullname) && fullname.includes(" ") && email.includes("@") && email.includes(".") && password.length >= 6 && password.length <= 12 ){
         const hasdPsw = await bcrypt.hash(password, 12);
         user = new User({
             fullname,
@@ -31,14 +31,37 @@ async function setNewUsser(req,res){
             user.admin = true;
         }
         await user.save();
+        console.log("user was sucssefully created");
         const date = new Date(); // Set the desired expiration date
         date.setDate(date.getDate() + 30); // For example, set the cookie to expire in 30 days
         res.cookie("myCookie", "", { expires: new Date(date) }); 
         res.redirect("/signin");
     }else{
-		res.redirect("/signup");
+        if (!fullname.includes(" ")) {
+            errorsArr.push("Last name required");
+            console.log("Last name required");
+        }
+        if (!/^[a-zA-Z\s]*[a-zA-Z][a-zA-Z\s]*$/.test(fullname)) {
+            errorsArr.push("Last name required");
+            console.log("Alphabet letter required");
+        }
+        if (!email.includes("@") || !email.includes(".")) {
+            errorsArr.push("Invalid Email");
+            console.log("Invalid Email");
+        }
+        if (password.value.length === 0 || password.length < 6 || password.length > 12) {
+            errorsArr.push("Password invalid");
+            console.log("Password invalid");
+        }
+        console.log("User was not created");
+		setTimeout(() => {
+            return res.redirect("/signup");
+        }, 2000);
+        //res.render("signup", { errorsArr });
     }
-
+    
 }
+
+
 
 module.exports = {getSignUpPage,setNewUsser};
